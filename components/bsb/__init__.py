a512e7299d3e7fa1bd7786daa63e2f066d0becdf
Arturo Casal <berfenger@gmail.com>
2026-02-06 00:46:33 +0100

    Fix component loading when using a subset of components

--- components/bsb/__init__.py
+++ components/bsb/__init__.py
@@ -12,7 +12,7 @@ CODEOWNERS = ["@eringerli"]
 MULTI_CONF = True
 
 DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["sensor", "text_sensor", "select", "button"] #, "switch", "binary_sensor"
+AUTO_LOAD = ["sensor", "text_sensor", "select", "button", "switch", "binary_sensor", "number", "time"]
 CONF_BSB_ID = "bsb_id"
 CONF_PARAMETER_NUMBER = "parameter_number"
 CONF_SOURCE_ADDRESS = "source_address"
